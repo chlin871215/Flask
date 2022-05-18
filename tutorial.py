c719dea103737dd31e4250adc07b9f2125702f4e
chlin871215 <barry123414@gmail.com>
2022-05-18 17:14:17 +0800

    updated response object

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,24 @@
 
+from unicodedata import name
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=["tim", "adda"])
+
+
+# @app.route("/<name>")
+# def user(name):
+#    return f"hello{name}!"
+
+
+# @app.route("/admin")
+# def admin():
+#    return redirect(url_for("user", name="Admin!"))
+
+
+if __name__ == "__main__":
+    app.run()
