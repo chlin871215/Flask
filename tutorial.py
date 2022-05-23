a046f756a557086904ebe88fe7386d7db559da64
chlin871215 <barry123414@gmail.com>
2022-05-23 17:50:31 +0800

    updated response object

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,24 @@
 
+from pickle import TRUE
 from unicodedata import name
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/<name>")
+@app.route("/")
 def home(name):
     return render_template("index.html", content=["tim", "adda"])
 
 
+@app.route('/login')
+def login():
+    return render_template()
+
+
+@app.route('/<usr')
+def user(usr):
+    return
 # @app.route("/<name>")
 # def user(name):
 #    return f"hello{name}!"
@@ -21,4 +30,4 @@ def home(name):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=TRUE)
